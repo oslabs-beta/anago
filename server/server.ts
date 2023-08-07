23c56650e43dd1ea9d3fcb9910b93680d253d015
Steve Schlepphorst <steve.schlepphorst@gmail.com>
2023-08-07 16:03:17 -0500

    dev fixes

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app: Express = express();
 const PORT: number = 3000;
 
-import dataRouter from './routers/dataRouter.js';
+// import dataRouter from './routers/dataRouter.js';
 import userRouter from './routers/userRouter.js';
 import { ServerError } from '../client/types.js';
 
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Route handlers
 // one visualization group array[0];
-app.use('/api/data', dataRouter);
+// app.use('/api/data', dataRouter);
 app.use('/api/user', userRouter);
 
 //Static handling for FULL BUILD ONLY (dev uses vite proxy);
@@ -46,16 +46,18 @@ app.use((req: Request, res: Response) => {
 //   message: { err: string };
 // };
 // Global error handler
-app.use((err: ServerError, req: Request, res: Response, next: NextFunction) => {
-  const defaultErr: ServerError = {
-    log: 'Error caught in global handler',
-    status: 400,
-    message: { err: 'An error occured' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+app.use(
+  (err: ServerError, _req: Request, res: Response, _next: NextFunction) => {
+    const defaultErr: ServerError = {
+      log: 'Error caught in global handler',
+      status: 400,
+      message: { err: 'An error occured' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}`);
