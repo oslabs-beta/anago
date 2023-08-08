9c5936b203f03909cfd3e1971dc3614cc6fd8816
Steve Schlepphorst <steve.schlepphorst@gmail.com>
2023-08-07 20:03:10 -0500

    first plot

--- client/Components/Dashboard.tsx
+++ client/Components/Dashboard.tsx
@@ -1,12 +1,29 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { JSX } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../stateStore';
 import MetricDisplay from './MetricDisplay';
 
 const Dashboard = () => {
-  let metrics: any[] = [];
+  const { currentMetrics }: any = useContext(StoreContext);
+  const [displayMetrics, setDisplayMetrics] = useState<JSX.Element[]>([]);
 
-  return <>{metrics}</>;
+  function printMetricDisplay() {
+    console.log(displayMetrics);
+    setTimeout(() => printMetricDisplay(), 5000);
+  }
+  setTimeout(() => printMetricDisplay(), 5000);
+
+  useEffect(() => {
+    const newDisplayMetrics = currentMetrics.map((el) => {
+      console.log('adding a metric display');
+      return <MetricDisplay metricId={el} />;
+    });
+    console.log('settingDisplayMetrics', newDisplayMetrics);
+    setDisplayMetrics(newDisplayMetrics);
+  }, [currentMetrics]);
+
+  return <div className="gallery-container">{displayMetrics}</div>;
 };
 
 export default Dashboard;
