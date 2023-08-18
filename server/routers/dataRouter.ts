df6b4c23d68ea36de30fbc7e6dfde6fd94f151ff
ryliep <133273675+ryliep@users.noreply.github.com>
2023-08-17 18:31:08 -0700

    Update dataRouter.ts- changed middleware routing for get request to /metrics endpoint

--- server/routers/dataRouter.ts
+++ server/routers/dataRouter.ts
@@ -5,17 +5,20 @@ const dataRouter = express.Router();
 
 //id = metric-id
 dataRouter.get(
+  // TODO: eventually reconfigure this because we will be sending metric id, along with other info, in the req body in the future
   '/metrics/:id',
   //metricsController.verifyMetric,
-  promApiController.getRangeMetrics,
+  promApiController.queryBuilder,
+  promApiController.getMetrics,
   async (_req: Request, res: Response) => {
     // make some data fetch
     // console.log(_req.body);
 
     return res.status(200).json(res.locals.promMetrics);
-  }
+  },
 );
 
 //does this metric exist? look at the searchQuery, make that request to promQL, parse the data, pass it to the front end
 
 export default dataRouter;
+
