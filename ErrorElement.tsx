5f097b7897a5c82febc073b863f41f7f5af535d9
Alexandra Ashcraft <lash211@gmail.com>
2023-08-22 18:22:45 -0700

    error element

---
+++ ErrorElement.tsx
@@ -0,0 +1,20 @@
+import { useRouteError } from 'react-router-dom';
+import React from 'react';
+
+const ErrorElement = () => {
+  let error = useRouteError();
+  console.error(error);
+  return (
+    <div className='error-handler'>
+      <div className='error-overlay'>
+        <img src='client/assets/images/shipwreck.png' id='shipwreck' />
+      </div>
+      <div className='error-message'>
+        <img src='client/assets/images/AnagoCrashed.png' id='error-logo' />
+        <h1>Oops! Page Not Found.</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorElement;
