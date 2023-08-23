d4cdb6192ee7a1e7911a64e96529d42cd3f8acf4
Alexandra Ashcraft <lash211@gmail.com>
2023-08-22 18:21:21 -0700

    controller global error handling added

--- server/controllers/k8sApiController.ts
+++ server/controllers/k8sApiController.ts
@@ -44,7 +44,12 @@ k8sController.getNodes = async (
     res.locals.cluster = { nodes: nodes };
     return next();
   } catch (error) {
-    console.log(error);
+    return next({
+      log: 'Error caught in k8sController getNodes', error,
+      status: 400,
+      message: { err: 'An error occured when fetching Node information from the Kubernetes API' },
+    })
+    
   }
 };
 
@@ -79,7 +84,11 @@ k8sController.getPods = async (
     res.locals.cluster = { pods: pods };
     next();
   } catch (error) {
-    console.log(error);
+    return next({
+      log: 'Error caught in k8sController getPods', error,
+      status: 400,
+      message: { err: 'An error occured when fetching Pod information from the Kubernetes API' },
+    })
   }
 };
 
@@ -108,7 +117,11 @@ k8sController.getNamespaces = async (
     res.locals.cluster = { namespaces: namespaces };
     next();
   } catch (error) {
-    console.log(error);
+    return next({
+      log: 'Error caught in k8sController getNamespaces', error,
+      status: 400,
+      message: { err: 'An error occured when fetching Namespace information from the Kubernetes API' },
+    })
   }
 };
 
@@ -139,7 +152,11 @@ k8sController.getServices = async (
     res.locals.cluster = { services: services };
     next();
   } catch (error) {
-    console.log(error);
+    return next({
+      log: 'Error caught in k8sController getServices', error,
+      status: 400,
+      message: { err: 'An error occured when fetching Service information from the Kubernetes API' },
+    })
   }
 };
 
@@ -170,7 +187,11 @@ k8sController.getDeployments = async (
     res.locals.cluster = { deployments: deployments };
     next();
   } catch (error) {
-    console.log(error);
+    return next({
+      log: 'Error caught in k8sController getDeployments', error,
+      status: 400,
+      message: { err: 'An error occured when fetching Deployment information from the Kubernetes API' },
+    })
   }
 };
 
@@ -196,7 +217,11 @@ k8sController.getCluster = async (
     res.locals.cluster = cluster;
     next();
   } catch (error) {
-    console.log(error);
+    return next({
+      log: 'Error caught in k8sController getCluster', error,
+      status: 400,
+      message: { err: 'An error occured when fetching Cluster information from the Kubernetes API' },
+    })
   }
 };
 
