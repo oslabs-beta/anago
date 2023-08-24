b650bcdd876c799e61fbc051fbe4d07de28e1534
Halia Haynes <haliahaynes@Halias-Mac.local>
2023-08-24 08:04:49 -1000

    setup page done

--- client/Routes/SetUp.tsx
+++ client/Routes/SetUp.tsx
@@ -1,59 +1,66 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { StoreContext } from '../context/stateStore';
+import React from 'react';
 
 const SetUp = () => {
   return (
-    <div>
-      <h1>Welcome to Anago!</h1>
-
-      <p>
-        Anago, or anágō, as properly styled in Ancient Greek, means to lead to a
-        higher place, to uplift, and to take to sea. In this spirit, Team Anago
-        hopes to serve as a perfect compass for your deployments on the
-        Kubernetes platform. (Kubernetes, as we know, is an Ancient Greek word
-        for 'captain', or 'helmsman'. Clearly one of our team members studied
-        Classics in university.) Let's dive in to how to set up and use our
-        platform so Anago can simplify and streamline managing your Kubernetes
-        deployments.
-      </p>
-
-      <img
-        src='../assets/images/anagoDef.png'
-        alt='Wikipedia definition for "anago"'
-      />
-
-      <ol className='table-of-contents'>
-        <h2>Table of Contents</h2>
-        <li>
-          <a>Getting Started</a>
-        </li>
-        <li>
-          <a>Configure Your Dashboard</a>
-        </li>
-        <li>
-          <a>How to Customize Metrics</a>
-        </li>
-        <li>
-          <a>Manage Your Alerts</a>
-        </li>
-        <li>
-          <a>Clusterview Visualizer</a>
-        </li>
-      </ol>
-
-      <h2>Getting Started</h2>
-      <p>
-        Team Anago is equipped for monitoring Kubernetes clusters hosted both on
-        local servers and on cloud-based platforms. Our primary cloud-support is
-        with the Amazon Web Services (AWS) Elastic Kubernetes Service (EKS)
-        platform, and we provide out of the box support for Prometheus API and
-        Prometheus Alertmanager integration with Chart.JS. Anago is meant to be
-        tech-stack agnostic, so let's walk you through how to connect any of
-        your Kubernetes deployments to Anago.
-      </p>
-      <ol>
-        <li></li>
-      </ol>
+    <div id='setup-page'>
+      <div id='setup-title'>
+        <h1 id='welcome'>Welcome to Anago!</h1>
+        <p>
+          An extensive tutorial for DevOps teams new to Kubernetes can be found
+          at our <a href='https://github.com/oslabs-beta/anago'>GitHub</a>. If
+          you have an existing cluster with Prometheus available, this set-up
+          walks through the swift installation and configuration of Anago.
+        </p>
+      </div>
+      <div id='setuplist'>
+        <div className='individSetup'>
+          <p> Follow these steps exactly:</p>
+          <p>
+            1. Clone the{' '}
+            <a href='https://github.com/oslabs-beta/anago'>Anago respository</a>{' '}
+            locally and install required modules:
+          </p>
+          <h4 className='npm'>npm install</h4>
+        </div>
+        <div className='individSetup' id='middle'>
+          <p>
+            2. Modify the{' '}
+            <a href='https://github.com/oslabs-beta/anago/blob/dev/user-config.ts'>
+              user-config.ts
+            </a>{' '}
+            file to point to access your Prometheus instance and Alertmanager.
+            For example, forward these services to the default ports (9090 and
+            9093):
+          </p>
+          <h4 className='npm'>
+            kubectl port-forward svc/[service-name] -n [namespace] 9090
+          </h4>
+          <h4 className='npm'>
+            kubectl port-forward svc/[service-name] -n [namespace] 9093
+          </h4>
+        </div>
+        <div className='individSetup'>
+          <p>3. Launch Anago from the command line:</p>
+          <h4 className='npm'>npm run dev</h4>
+        </div>
+      </div>
+      <div id='localAccess'>
+        <p>
+          Navigate to the local access point for Vite (by default,{' '}
+          <a href='/'>http://locahost:5173</a>), and you should see a default
+          Anago dashboard populated with a suite of common metrics!
+        </p>
+        <p>
+          {' '}
+          <strong>
+            All set?{' '}
+            <a href='/'>
+              Explore your new dashboard and gain insight in your Kubernetes
+              cluster!
+            </a>
+          </strong>
+        </p>
+      </div>
     </div>
   );
 };
